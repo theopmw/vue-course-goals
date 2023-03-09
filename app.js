e853189a7cf0a6c48a93537a1046d42979ef2d0d
theopmw <theowrightdev@gmail.com>
2023-03-09 14:16:50 +0000

    Add methods

--- app.js
+++ app.js
@@ -1,9 +1,20 @@
 const app = Vue.createApp({
   data() {
     return {
-      courseGoal: 'Finish the course and learn Vue',
+      courseGoalA: 'Finish the course and learn Vue',
+      courseGoalB: 'Master Vue',
       vueLink: 'https://vuejs.org/',
     };
+  },
+  methods: {
+    outputGoal() {
+      const randomNumber = Math.random();
+      if (randomNumber < 0.5) {
+        return this.courseGoalA;
+      } else {
+        return this.courseGoalB;
+      }
+    },
   },
 });
 
